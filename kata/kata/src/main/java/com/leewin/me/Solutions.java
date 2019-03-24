ecb82cdda18ccd851823acee69597691f07fa664
Leewin <leewin.zhang@gmail.com>
2019-03-24 11:40:32 +0000

    [#11][Leewin] Solved problem 18

--- kata/kata/src/main/java/com/leewin/me/Solutions.java
+++ kata/kata/src/main/java/com/leewin/me/Solutions.java
@@ -235,4 +235,26 @@ class Solutions {
         }
         return result.longValue();
     }
+
+    static long findBiggestValuePathFrom(String input) {
+        int[][] triangle = convertToTriangle(input);
+        int height = triangle.length;
+        for (int i = height - 2; i >= 0; i--) {
+            for (int j = 0; j <= i; j++) {
+                triangle[i][j] += Math.max(triangle[i + 1][j], triangle[i + 1][j + 1]);
+            }
+        }
+        return triangle[0][0];
+    }
+
+    private static int[][] convertToTriangle(String input) {
+        return Stream
+                .of(input.split("\\n"))
+                .map(s -> Stream
+                        .of(s.split("\\s"))
+                        .mapToInt(Integer::valueOf)
+                        .toArray()
+                )
+                .toArray(int[][]::new);
+    }
 }
